58f55dc53a463ff04e93584b523672e169c26e1c
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-07 18:56:35 -0600

    Create app.js. Create generateHTML(), generateHTML(). Layout raw images on index.html.

---
+++ Wes-Bos-CSSGrid/Grid-Image-Gallery/app.js
@@ -0,0 +1,35 @@
+const gallery = document.querySelector('.gallery');
+const overlay = document.querySelector('.overlay');
+
+// images, updated when clicked, close button
+const overlayImage = overlay.querySelector('img');
+const overlayClose = overlay.querySelector('.close');
+
+//generate HTML based upon passed in widths and height
+function generateHTML([h, v]) {
+	return `
+		<div class="item h${h} v${v}">
+			<img src="images/${generateRandomNumber(12)}.jpg">
+			<div class="item__overlay">
+				<button>View -></button>
+			</div>
+		</div>
+	`;
+}
+
+	function generateRandomNumber(limit) {
+		return Math.floor(Math.random() * limit) + 1
+	}
+
+	// Array.from can take a length property
+	// map the generated numbers in the array
+	const digits = Array.from({length: 50}, () =>
+	[generateRandomNumber(4), generateRandomNumber(4)]);
+	console.log(digits);
+
+	// for each index, the values are passed into generateHTML
+	const html = digits.map(generateHTML).join('');
+	console.log(html);
+
+	// display the items on the page;
+	gallery.innerHTML = html;
