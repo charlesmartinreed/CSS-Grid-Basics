36807103cdf2d643ce70e0cab73528846ca2e7fc
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-07 19:38:20 -0600

    Complete CSS image grid project.

--- Wes-Bos-CSSGrid/Grid-Image-Gallery/app.js
+++ Wes-Bos-CSSGrid/Grid-Image-Gallery/app.js
@@ -21,15 +21,35 @@ function generateHTML([h, v]) {
 		return Math.floor(Math.random() * limit) + 1
 	}
 
+	function handleClick(e) {
+		// PER MDN: currentTarget always refers to the element to which the event handler has been attached, while event.target identifies the element on which the event occurs
+
+		//look for the img inside of the current target and get its src
+		// display it in the overlay image
+		const src = e.currentTarget.querySelector('img').src;
+		overlayImage.src = src;
+		overlay.classList.add('open')
+	}
+
+	overlayClose.addEventListener('click', close);
+
+	function close() {
+		overlay.classList.remove('open');
+	}
+
 	// Array.from can take a length property
 	// map the generated numbers in the array
 	const digits = Array.from({length: 50}, () =>
 	[generateRandomNumber(4), generateRandomNumber(4)]).concat([[1,1], [1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]);
-	console.log(digits);
+	//console.log(digits);
 
 	// for each index, the values are passed into generateHTML
 	const html = digits.map(generateHTML).join('');
-	console.log(html);
+	//console.log(html);
 
 	// display the items on the page;
 	gallery.innerHTML = html;
+
+	//when item is clicked, show the overlay
+	const items = document.querySelectorAll('.item');
+	items.forEach(item => item.addEventListener('click', handleClick));
